282d0354ffb8a0ae8ff30cecb7af46cbfd019ad5
EC2 Default User <ec2-user@ip-172-31-21-146.ec2.internal>
2020-09-19 04:05:56 +0000

    Twitter quotes pulled infinitely

--- twitter_quotes.py
+++ twitter_quotes.py
@@ -1,32 +1,45 @@
-from tweepy import Stream
-from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-import time
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy import API
+from tweepy import Cursor
 import os
 
 api_key = os.environ["API_KEY"]
 api_secret_key = os.environ["API_SECRET_KEY"]
 access_token = os.environ["ACCESS_TOKEN"]
 access_token_secret  = os.environ["ACCESS_TOKEN_SECRET"]
- 
-class listener(StreamListener):
-    
+        
+class my_listener(StreamListener):
+    def __init__(self, fetched_tweets):
+        self.fetched_tweets = fetched_tweets
+        
     def on_data(self, data):
-
-        tweet=data.split(',"text":"')[1].split('","source')[0]
-        print(tweet+"\n")
-        savefile=str(time.time())+"::"+tweet
-        save=open('twitter_list.csv','a')
-        save.write(savefile)
-        save.write("\n\n")
-        save.close()
+        try:
+            print(data)
+            with open(self.fetched_tweets, "a") as ft:
+                ft.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
         return True
 
     def on_error(self, status):
-        print (status)
+        print(status)
+        
+class TwitterStreamer():
+    def stream_tweets(self, fetched_tweets, keywords):
+        listener = my_listener(fetched_tweets)
+        auth = OAuthHandler(api_key, api_secret_key)
+        auth.set_access_token(access_token, access_token_secret)
  
-auth = OAuthHandler(api_key, api_secret_key)
-auth.set_access_token(access_token, access_token_secret)
+        twitterStream = Stream(auth, listener)
+        twitterStream.filter(track=["pasta"])
  
-twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["pasta"])
+if __name__ == "__main__":
+    keywords = ["pasta"]
+    fetched_tweets = "tweets.json"
+    
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets, keywords)
+    
